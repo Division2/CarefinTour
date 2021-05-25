33ebc7c4e9142bd32a52f862c7ae5c0847b494fb
박성동 <freegunz2@naver.com>
2021-05-25 16:33:09 +0900

    log4j 오류 수정

--- src/main/java/com/spring/ex/service/BannerService.java
+++ src/main/java/com/spring/ex/service/BannerService.java
@@ -2,13 +2,18 @@ package com.spring.ex.service;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.spring.ex.vo.BannerVO;
 
 public interface BannerService {
-	//여행 포토 수정(사진 & 내용)
-	//public void TravelPhotoModify(TravelPhotoVO travelPhotoVO, String[] files, String[] fileNames, MultipartHttpServletRequest mpRequest) throws Exception;
+	
+	//배너수정	
+	public int BannerModify(Map<String, Object> map) throws Exception;
 	
 	//배너 출력
-	public List<BannerVO> asd (HashMap<String, Integer> map) throws Exception;
+	public List<BannerVO> BannerView(HashMap<String, Integer> map) throws Exception;
+
+	//여행패키지 파일이름 검색 - 파일 삭제하려고 사용
+	public BannerVO BannerFileName(int bid) throws Exception;
 }
