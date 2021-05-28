1980319f1b7f8befbed33ff4899236deaf06b25d
박성동 <freegunz2@naver.com>
2021-05-28 21:40:56 +0900

    여행 포토 조금 수정, 메인 페이지 최신 인기 여행리뷰 3개 출력(최근 7일간 조회수가 높은 게시글)

--- src/main/java/com/spring/ex/dao/MainPageDAO.java
+++ src/main/java/com/spring/ex/dao/MainPageDAO.java
@@ -1,8 +1,14 @@
 package com.spring.ex.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
+import com.spring.ex.vo.TravelPhotoVO;
 
 @Repository
 public interface MainPageDAO {
 	
+	//여행 포토 출력
+	public List<TravelPhotoVO> NewTravelPhotoList() throws Exception;
 }
--- src/main/java/com/spring/ex/service/MainPageServiceImpl.java
+++ src/main/java/com/spring/ex/service/MainPageServiceImpl.java
@@ -1,12 +1,21 @@
 package com.spring.ex.service;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
 import com.spring.ex.dao.MainPageDAO;
+import com.spring.ex.vo.TravelPhotoVO;
 
 @Service
 public class MainPageServiceImpl implements MainPageService {
 	@Inject MainPageDAO dao;
+	
+	//여행 포토 출력
+	@Override
+	public List<TravelPhotoVO> NewTravelPhotoList() throws Exception {
+		return dao.NewTravelPhotoList();
+	}
 }
